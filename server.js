d3adf0c12c299fbc7e36716aea6020a18e2d6c08
JoaoCorreio <joaocnp11@gmail.com>
2025-06-10 17:22:27 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+const USERS_FILE = './users.json';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/register', (req, res) => {
+  const { name, email, senha } = req.body;
+  const usuarios = JSON.parse(fs.readFileSync(USERS_FILE, 'utf-8'));
+
+  const existente = usuarios.find(user => user.email === email);
+  if (existente) {
+    return res.redirect('/register.html');
+  }
+
+  usuarios.push({ name, email, senha });
+  fs.writeFileSync(USERS_FILE, JSON.stringify(usuarios, null, 2));
+  res.redirect('/index.html');
+});
+
+app.post('/login', (req, res) => {
+  const { email, senha } = req.body;
+  const usuarios = JSON.parse(fs.readFileSync(USERS_FILE, 'utf-8'));
+
+  const user = usuarios.find(u => u.email === email && u.senha === senha);
+  if (user) {
+    res.redirect('/index.html'); 
+  } else {
+    res.redirect('/login.html'); 
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
